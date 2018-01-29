bc34e0870eb927162154c401bcf8c181912693b1
Vincent <vmorvan@student.42.fr>
2018-01-29 15:23:51 +0100

    The packages install and convert bulma-extensions now

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp')
 const replace = require('gulp-replace')
+const insert = require('gulp-insert')
 const converter = require('sass-convert')
 const runSequence = require('run-sequence')
 const fs = require('fs')
@@ -25,11 +26,36 @@ gulp.task('bulmaConvert1', () => {
       }))
       .pipe(replace('.sass"', '.scss"'))
       .pipe(replace('sass/', 'scss/'))
+      .pipe(insert.append('@import "scss/extensions/extensions";'))
       .pipe(gulp.dest('./'))
 })
 
+gulp.task('bulma-extensions2', () => {
+  return gulp.src('../bulma-extensions/*/*')
+    .pipe(converter({
+      from: 'sass',
+      to: 'scss',
+      rename: true
+    }))
+    .pipe(replace('.sass"', '.scss"'))
+    .pipe(replace('sass/', 'scss/'))
+    .pipe(gulp.dest('./scss/extensions'))
+})
+
+gulp.task('bulma-extensions1', () => {
+  return gulp.src('../bulma-extensions/extensions.sass')
+    .pipe(converter({
+      from: 'sass',
+      to: 'scss',
+      rename: true
+    }))
+    .pipe(replace('.sass"', '.scss"'))
+    .pipe(replace('sass/', 'scss/'))
+    .pipe(gulp.dest('./scss/extensions'))
+})
+
 gulp.task('bulma', done => {
-  runSequence('bulmaConvert1', 'bulmaConvert2', () => {
+  runSequence('bulmaConvert1', 'bulmaConvert2', 'bulma-extensions1', 'bulma-extensions2', () => {
     done()
   })
 })
