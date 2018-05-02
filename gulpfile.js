1dddf19c71eb3d57b89968eac583ab5a73c0e7c3
Vincent Morvan <mvinc@MacBook-Pro-de-Vincent.local>
2018-05-02 15:40:07 +0200

    Update bulma and bulma-extension

--- gulpfile.js
+++ gulpfile.js
@@ -25,24 +25,11 @@ gulp.task('bulmaConvert1', () => {
       }))
       .pipe(replace('.sass"', '.scss"'))
       .pipe(replace('sass/', 'scss/'))
-      .pipe(insert.append('@import "scss/extensions/extensions";'))
       .pipe(gulp.dest('./'))
 })
 
-gulp.task('bulma-extensions2', () => {
-  return gulp.src('../bulma-extensions/*/*')
-    .pipe(converter({
-      from: 'sass',
-      to: 'scss',
-      rename: true
-    }))
-    .pipe(replace('.sass"', '.scss"'))
-    .pipe(replace('sass/', 'scss/'))
-    .pipe(gulp.dest('./scss/extensions'))
-})
-
-gulp.task('bulma-extensions1', () => {
-  return gulp.src('../bulma-extensions/extensions.sass')
+gulp.task('bulma-extensions', () => {
+  return gulp.src('../bulma-extensions/*/*/*')
     .pipe(converter({
       from: 'sass',
       to: 'scss',
@@ -54,7 +41,7 @@ gulp.task('bulma-extensions1', () => {
 })
 
 gulp.task('bulma', done => {
-  runSequence('bulmaConvert1', 'bulmaConvert2', 'bulma-extensions1', 'bulma-extensions2', () => {
+  runSequence('bulmaConvert1', 'bulmaConvert2', 'bulma-extensions', () => {
     done()
   })
 })
